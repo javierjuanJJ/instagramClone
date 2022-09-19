401c718cd3e559e7a1336b0dc5294386918f3e3e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-19 10:17:15 +0200

    Commit 4: Responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
       title: 'Instagram clone',
       home: Scaffold(
         body: Text('Lets build a Instagram clone'),
