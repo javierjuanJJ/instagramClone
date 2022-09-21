1a7c20125b5a05c4b8105a59602a2f62046602fe
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-21 11:33:56 +0200

    Commit 15: Post card

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+import '../widgets/post_card.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/ic_instagram.png',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.messenger_outline)),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => PostCard(
+              snap: snapshot.data!.docs[index],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
 
 import '../screens/add_post_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreens = [
-  Text('a'),
+  FeedScreen(),
   Text('b'),
   AddPostScreen(),
   Text('d'),
